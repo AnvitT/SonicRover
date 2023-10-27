5875aa146256a00e5684c14cb800602317e90db0
AnvitT <99025362+AnvitT@users.noreply.github.com>
2023-10-27 05:34:24 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,10 +1,14 @@
 #include <AFMotor.h>
 #include <SoftwareSerial.h>
 #include <NewPing.h>
+#include <NewTone.h>
 
 SoftwareSerial BTSerial(A4, A5);
+
 #define TRIGGER_PIN A0
 #define ECHO_PIN A1
+
+#define TONE_PIN A3
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN);
 
@@ -13,37 +17,21 @@ AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
-#define FORWARD_COMMAND 'F'
-#define STOP_COMMAND 'S'
-#define OBSTACLE_DISTANCE 12
-#define MAX_SPEED 100
-
-void setupMotorsSpeed() {
-  motor1.setSpeed(MAX_SPEED);
-  motor2.setSpeed(MAX_SPEED);
-  motor3.setSpeed(MAX_SPEED);
-  motor4.setSpeed(MAX_SPEED);
-}
-
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   BTSerial.begin(9600);
-  setupMotorsSpeed();
-}
-
-void debounce() {
-  delay(10);
+  motor1.setSpeed(80);
+  motor2.setSpeed(80);
+  motor3.setSpeed(80);
+  motor4.setSpeed(80)..
+  0;
 }
 
 void forward(){
-    if (sonar.ping_cm() < OBSTACLE_DISTANCE)
-        stop();
-    else {
-        motor1.run(FORWARD);
-        motor2.run(FORWARD);
-        motor3.run(FORWARD);
-        motor4.run(FORWARD);
-    }
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
 }
 
 void stop(){
@@ -53,33 +41,51 @@ void stop(){
     motor4.run(RELEASE);
 }
 
-bool checkValid(char command) {
-  return command == FORWARD_COMMAND || command == STOP_COMMAND;
+bool checkValid(char temp){
+  char valid[] = {'F','S'};
+  bool isValid = false;
+  for (int i = 0; i < sizeof(valid);i++){
+    if (temp == valid[i]){
+      isValid = true;
+      break;
+    }
+  }
+  return isValid;
+}
+
+char data;
+
+void checkObstacle(){
+  unsigned int distance = sonar.ping_cm();
+  if (distance < 12 && data == 'F'){
+    stop();
+    NewTone(TONE_PIN,300);
+    delay(100);
+    noNewTone(TONE_PIN);
+    // Serial.print("Obstacle detected at ");
+    // Serial.print(distance);
+    // Serial.println("cm!! Stopping...");
+  }
 }
 
 void loop() {
-  char command;
   if (BTSerial.available()) {
-    command = BTSerial.read();
-    debounce();
-    if(checkValid(command)) {
-      switch(command) {
-        case FORWARD_COMMAND:
-            forward();
-            Serial.println("Forward!");
-            break;
-            
-        case STOP_COMMAND:
-            stop();
-            Serial.println("Stopping!");
-            break;
-        }
+    data = BTSerial.read();
+    delay(500);
+    // Serial.println(data);
+  }
+  if (checkValid(data)){
+    if (data == 'F'){
+      forward();
+      // Serial.println("Forward!");
     }
-    else {
-      Serial.println("Invalid Command!");
+    else if (data == 'S'){
+      stop();
+      // Serial.println("Stopping!");
+    }
+    else{
+      // Serial.println("Invalid Command!");
     }
   }
-
-  while(BTSerial.available() > 0) BTSerial.read(); 
-  while(Serial.available() > 0) Serial.read(); 
-}
+  checkObstacle();
+  }
